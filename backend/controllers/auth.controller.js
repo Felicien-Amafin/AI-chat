fc1b71720f25a18c922c6b3c6c1e68363c0f7591
Felicien Amafin <felicien.amafin@gmail.com>
2025-06-20 19:20:17 +0200

    BACKEND: Completing verifyEmail in auth controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -28,20 +28,20 @@ export const signup = async (req, res) => {
         
         const hashedPsswd = await encryptPassword(password);
        
-        const verificationCode = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
+        const emailVerifCode = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
     
         const newUser = new User({
             username,
             email,
             password: hashedPsswd,
-            verificationCode
+            emailVerifCode
         });
         await newUser.save();
 
         sendVerificationEmail(
             `${process.env.CLIENT_URL}/auth/email-verification/${newUser._id}`, 
             newUser.email, 
-            verificationCode
+            emailVerifCode
         );
         
         return res.status(201).json({
@@ -53,7 +53,62 @@ export const signup = async (req, res) => {
         });
 
     } catch(error) {
-        console.log(error)
+        return res.status(500).json({ 
+            message: 'Erreur de serveur.', 
+        });
+    }
+}
+
+export const verifyEmail = async (req, res) => {
+    //Verifies user's email via a unique code 
+    const userId = req.params.userId;
+    const { code } = req.body;
+    const USER_ID_LENGTH = 24;
+
+    try {
+        if (userId.length != USER_ID_LENGTH) {
+            return res.status(400).json({ 
+                message: 'Identifiant utilisateur invalide',
+            });
+        }
+    
+        const user = await User.findOne({ _id: userId });
+       
+        if (!user) {
+             return res.status(404).json({ 
+                message: 'Identifiant utilisateur invalide',
+            });
+        }
+    
+        if (user.isEmailVerified && (code === user.emailVerifCode)) {
+            return res.status(200).json({
+                message: 'Votre adresse email a déjà été confirmée. vous pouvez maintenant vous connecter',
+                user: {
+                    _id: user._id,
+                    user: user.username
+                }
+            })
+        } 
+    
+        if (!code || (code !== user.emailVerifCode)) {
+            return res.status(400).json({ 
+                message: 'Code de vérification invalide. Utilisez le code envoyé à votre adresse email.',
+                errors: { code: 'Code de vérification invalide.'}
+            });
+        }
+    
+        user.isEmailVerified = true;
+        user.save();
+    
+        return res.status(200).json({
+            message: 'Votre adresse email a été confirmée avec succès! Vous pouvez maintenant vous connecter.',
+            user: {
+                _id: user._id,
+                user: user.username
+            }
+        });
+
+    } catch (error) {
         return res.status(500).json({ 
             message: 'Erreur de serveur.', 
         });
