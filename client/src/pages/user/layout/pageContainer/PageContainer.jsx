890cf352d56ef3883b0e52f1fe96255befd0ff89
Felicien Amafin <felicien.amafin@gmail.com>
2025-07-28 20:03:47 +0200

    FRONTEND: Removing useVeriftAccessToken + useFetchAuthorization

--- client/src/pages/user/layout/pageContainer/PageContainer.jsx
+++ client/src/pages/user/layout/pageContainer/PageContainer.jsx
@@ -1,23 +1,17 @@
-import useVerifyAccesTk from '../../hooks/useVerifyAccesTk';
 import Loader from '../../../../components/others/Loader';
 import style from './pageContainer.module.css';
-import usePersistLogin from '../../../../hooks/usePersistLogin';
-import useLogout from '../../hooks/useLogout';
 
 const PageContainer = ({children}) => {
-  const { isExpired } = useVerifyAccesTk();
-  const { isAuthPending, isUnAuthorized } = usePersistLogin(isExpired);
-  useLogout(isUnAuthorized);
-  console.log('page container executing...')
+  
   return (
-    <div className={`${style.pageContainer} bckGroundImg`}>
+    {/* <div className={`${style.pageContainer} bckGroundImg`}>
       {(isExpired && isAuthPending) && 
         <div className={`${style.loader} flexColumn-allCentered`}>
           <Loader size={60} color='white'/>
         </div>
       }
       {!isExpired && <>{children}</>}
-    </div>
+    </div> */}
   )
 }
 
