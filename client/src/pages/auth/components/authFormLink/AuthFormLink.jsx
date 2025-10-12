2e075bf0bb10b56acdf295c79bc26439e388de0e
Felicien Amafin <felicien.amafin@gmail.com>
2025-10-12 22:10:08 +0200

    Fixing module import error

--- client/src/pages/auth/components/authFormLink/AuthFormLink.jsx
+++ client/src/pages/auth/components/authFormLink/AuthFormLink.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import style from './authformLink.module.css';
+import style from './authFormLink.module.css';
 
 const AuthFormLink = ({children, path}) => {
   return (
